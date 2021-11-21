041f30f7287395fdd30e9ac2c8a298e60f8e8b6e
Steph Peets <stephanie.peets@gmail.com>
2021-11-21 14:58:59 -0500

    complete model validation reading

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,5 +1,8 @@
 package org.launchcode.codingevents.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Objects;
 
 /**
@@ -7,54 +10,78 @@ import java.util.Objects;
  */
 public class Event {
 
-    private int id;
-    private static int nextId = 1;
+	private int id;
+	private static int nextId = 1;
 
-    private String name;
-    private String description;
+	@NotBlank(message = "Name is required.")
+	@Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters.")
+	private String name;
 
-    public Event(String name, String description) {
-        this.name = name;
-        this.description = description;
-        this.id = nextId;
-        nextId++;
-    }
+	@Size(max = 500, message = "Description must be 500 characters or less.")
+	private String description;
 
-    public String getName() {
-        return name;
-    }
+	@NotBlank(message = "Contact email is required.")
+	@Email(message = "Invalid email. Please Try again.")
+	private String contactEmail;
 
-    public void setName(String name) {
-        this.name = name;
-    }
 
-    public String getDescription() {
-        return description;
-    }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+	public Event(String name, String description, String contactEmail) {
+		this.name = name;
+		this.description = description;
+		this.contactEmail = contactEmail;
+		this.id = nextId;
+		nextId++;
+	}
 
-    public int getId() {
-        return id;
-    }
+	public Event() {
+		this.id = nextId;
+		nextId++;
+	}
 
-    @Override
-    public String toString() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Event event = (Event) o;
-        return id == event.id;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getContactEmail() {
+		return contactEmail;
+	}
+
+	public void setContactEmail(String contactEmail) {
+		this.contactEmail = contactEmail;
+	}
+
+	@Override
+	public String toString() {
+		return name;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Event event = (Event) o;
+		return id == event.id;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
 }
